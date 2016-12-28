29617304ab337d29c9147e540a719bc7de12bd04
yummy1121@foxmail.com <yummy1121@foxmail.com>
2016-12-28 22:20:09 +0800

    finish game

--- index.ios.js
+++ index.ios.js
@@ -35,7 +35,10 @@ export default class AwesomeProject extends Component {
 		super(props);
 
 		this._startShake = this._startShake.bind(this);
+		this._setChoose = this._setChoose.bind(this);
+		this._checkChoose = this._checkChoose.bind(this);
 		this._setGameModel = this._setGameModel.bind(this);
+		this._setNum = this._setNum.bind(this);
 		this._showAccount = this._showAccount.bind(this);
 		this._renderGameImage = this._renderGameImage.bind(this);
 		this._renderAImage = this._renderAImage.bind(this);
@@ -51,6 +54,8 @@ export default class AwesomeProject extends Component {
 
 		this._model = true;
 		this._account = 2000; //初始金额
+		this._num = -1;
+		this._choose = false;
 		this._rotate = 0; //遥感旋转角度
 		this._time = null; //计算时间
 		this._prizetext = ''; //中奖显示的信息
@@ -65,6 +70,12 @@ export default class AwesomeProject extends Component {
 	componentDidMount() {
 		//组件加载完后就让文字动
 		this._startWordAnimate(0.05);
+	}
+	_setNum(num) {
+		this._num = num;
+		this.setState({
+			_num: num
+		});
 	}
 	_rebackShake() {
 		//回去
@@ -136,16 +147,20 @@ export default class AwesomeProject extends Component {
 	}
 	_startShake() {
 		//摇奖点击
-		if (this._prizenum === 3) {
-			this._prizenum = 0;
-			Animated.timing(this.state.transformYValue, {
-				toValue: 20,
-				duration: 500,
-			}).start();
+		if((!this._choose) && this._model) {
+			Alert.alert('温馨提示', '请先选择你要下注的水果');
+		} else {
+			if (this._prizenum === 3) {
+				this._prizenum = 0;
+				Animated.timing(this.state.transformYValue, {
+					toValue: 20,
+					duration: 500,
+				}).start();
 
-			this._time = Date.now();
-			this._rotateAnimated();
-			this._startScroll();
+				this._time = Date.now();
+				this._rotateAnimated();
+				this._startScroll();
+			}
 		}
 	}
 	_refSet(refer, props) {
@@ -159,7 +174,8 @@ export default class AwesomeProject extends Component {
 		})
 	}
 	_getPrize() { //获奖
-		const itemArr1 = ["","",""];
+		const num = this._num;
+		const itemArr1 = ['', 50, 100, 200];
 		const itemArr2 = {
 			item1: ["通讯录第六位", "在场名字中带N的", "左手边第三位"],
 			item2: ["躺在桌子上", "一边唱征服一边", "站在凳子上"],
@@ -168,16 +184,39 @@ export default class AwesomeProject extends Component {
 
 		let prizeArr = this._prizearr;
 		let result = '';
+		let res = 0;
+		let add = 0;
 		if (this._model) {
-			result = ''
+			for(let i=0; i < prizeArr.length; i++) {
+				if(prizeArr[i] == num){
+					res++;
+				}
+			}
+			if(res == 1) {
+				add = 0.5 * itemArr1[num];
+			} else if(res == 2) {
+				add = 1 * itemArr1[num];
+			} else if(res == 3) {
+				add = 3 * itemArr1[num];
+			} else {
+
+			}
+			result = '押注中' + res + '个，获得：￥' + add;
+			Alert.alert(result, '请注意查收余额', [
+				{text: '确定', onPress: () => console.log('OK Pressed!')},
+			]);
 		} else {
-			result = itemArr2.item1[prizeArr[0]-1] + itemArr2.item2[prizeArr[1]-1] + itemArr2.item3[prizeArr[2]-1];
+			result = '和' + itemArr2.item1[prizeArr[0]-1] + itemArr2.item2[prizeArr[1]-1] + itemArr2.item3[prizeArr[2]-1];
 		}
+
+		const addAccount = this._account + add;
+		this._account = addAccount;
 		this._prizetext = result;
 		this.setState({
-			_prizetext: result
+			_prizetext: result,
+			_account: addAccount
 		});
-		console.warn('中奖序列', this._prizearr, result);
+		//console.warn('中奖序列', this._prizearr);
 	}
 	_setGameModel() {
 		const currentModel = this._model;
@@ -295,12 +334,38 @@ export default class AwesomeProject extends Component {
 		setTimeout(this._startScrollActurally, 2000, 3, 1, cyclecount);
 	}
 	_showAccount() {
-		Alert.alert('当前余额为：￥' + this._account);
+		Alert.alert('当前余额为：￥' + this._account, null, [
+			{text: '确定', onPress: () => console.log('OK Pressed!')},
+		]);
+	}
+	_checkChoose() {
+		const arr1 = ['', '你选择了樱桃', '你选择了柠檬', '你选择了菠萝'];
+		const arr2 = ['', '50', '100', '200'];
+		if(this._num == 0) {
+			alert('你还没有任何下注');
+		} else {
+			const self = this;
+
+			Alert.alert(arr1[this._num], '将会扣取你￥' + arr2[this._num], [
+				{text: '取消', onPress: () => console.log('OK Pressed!')},
+				{text: '确认下注', onPress: () => self._setChoose()},
+			]);
+		}
+	}
+	_setChoose() {
+		const arr = ['', 50, 100, 200];
+		const currentAccount = this._account - arr[this._num];
+		this._choose = true;
+		this._account = currentAccount;
+		this.setState({
+			_choose: true,
+			_account: currentAccount
+		});
 	}
 	render() {
 		return (
 			<View style={styles.background}>
-		        <View style={styles.container}>
+				<View style={styles.container}>
 					<View>
 						<Text style={styles.title}>疯狂老虎机</Text>
 					</View>
@@ -308,27 +373,57 @@ export default class AwesomeProject extends Component {
 						<Text style={styles.account} onPress={this._showAccount}>￥{this._account}</Text>
 					</View>
 				</View>
-        		<Image source={require('./assets/background.png')} style={styles.scene}>
-			        <View>
-				        <TouchableOpacity onPress={() => alert('a')} activeOpacity={1}>
-					        <Image source={require('./assets/choose_btn_1.png')} style={styles.choosebtn1}/>
-				        </TouchableOpacity>
-			        </View>
-			        <View>
-				        <TouchableOpacity onPress={() => alert('b')} activeOpacity={1}>
-					        <Image source={require('./assets/choose_btn_2.png')} style={styles.choosebtn2}/>
-				        </TouchableOpacity>
-			        </View>
-			        <View>
-				        <TouchableOpacity onPress={() => alert('c')} activeOpacity={1}>
-					        <Image source={require('./assets/choose_btn_3.png')} style={styles.choosebtn3}/>
-				        </TouchableOpacity>
-			        </View>
-			        <View>
-				        <TouchableOpacity onPress={() => alert('start')} activeOpacity={1}>
-					        <Image source={require('./assets/choose_ok.png')} style={styles.chooseend}/>
-				        </TouchableOpacity>
-			        </View>
+				<Image source={require('./assets/background.png')} style={styles.scene}>
+					{   this._model ?
+						<View>
+							<View>
+								<TouchableOpacity onPress={() => this._setNum(1)} activeOpacity={1}>
+									<Image source={require('./assets/choose_btn_3.png')} style={styles.choosebtn1}/>
+								</TouchableOpacity>
+							</View>
+							<View>
+								<TouchableOpacity onPress={() => this._setNum(2)} activeOpacity={1}>
+									<Image source={require('./assets/choose_btn_1.png')} style={styles.choosebtn2}/>
+								</TouchableOpacity>
+							</View>
+							<View>
+								<TouchableOpacity onPress={() => this._setNum(3)} activeOpacity={1}>
+									<Image source={require('./assets/choose_btn_2.png')} style={styles.choosebtn3}/>
+								</TouchableOpacity>
+							</View>
+							<View>
+									{ this._num == -1 &&
+										<Text style={styles.notice}>
+											你还没有选择下注的水果
+										</Text>
+									}
+									{ this._num == 1 &&
+										<Text style={styles.notice}>
+											你选择了樱桃
+										</Text>
+									}
+									{ this._num == 2 &&
+										<Text style={styles.notice}>
+											你选择了柠檬
+										</Text>
+									}
+									{ this._num == 3 &&
+										<Text style={styles.notice}>
+											你选择了菠萝
+										</Text>
+									}
+							</View>
+							<View>
+								<TouchableOpacity onPress={this._checkChoose} activeOpacity={1}>
+									<Image source={require('./assets/choose_ok.png')} style={styles.chooseend}/>
+								</TouchableOpacity>
+							</View>
+						</View>
+						:
+						<View>
+							<Text style={styles.notice}>大冒险模式</Text>
+						</View>
+					}
 					<View style={styles.prizeScrollContainer}>
 						<View style={{flexDirection:'row',justifyContent:'space-between',position:'absolute',left:0,height:40,alignItems:'center',marginRight: 20,backgroundColor:'transparent',transform:[{translateX:WORDPOSITION}]}} ref={WORDCONTAINER}>
 							<Text style={styles.word}>{this._prizetext}</Text>
@@ -395,7 +490,7 @@ export default class AwesomeProject extends Component {
 						<Animated.Image source={require('./assets/btn_start.png')} style={[styles.starbtn,{transform:[{translateY:this.state.transformYValue},{rotateX:this.state.rotateXValue +'deg'}]}]} />
 					</TouchableOpacity>
 				</Image>
-      		</View>
+			</View>
 		);
 	}
 }
@@ -453,19 +548,25 @@ const styles = StyleSheet.create({
 		width: windowWidth * 0.30,
 		height: windowHeight * 0.06,
 	},
+	notice: {
+		position: 'relative',
+		color: '#FFFFFF',
+		textAlign: 'center',
+		top: windowHeight * 0.108,
+		fontSize: 16,
+	},
 	chooseend: {
 		position: 'absolute',
-		marginTop: windowHeight * 0.165,
+		marginTop: windowHeight * 0.146,
 		width: windowWidth,
 		height: windowHeight * 0.056,
-		//backgroundColor: '#FFFFFF',
 	},
 	starbtn: {
 		position: 'absolute',
 		width: windowWidth * 0.09375,
 		height: windowHeight * 0.1080,
 		left: windowWidth * 0.8135,
-		top: windowHeight * 0.4035,
+		top: windowHeight * 0.39,
 	},
 	prizeBingo: {
 		position: 'absolute',
